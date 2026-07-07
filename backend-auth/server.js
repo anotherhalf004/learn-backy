4085086f0af6664fd8320e866d63986db598c5d4
sanskar <191781814+anotherhalf004@users.noreply.github.com>
2026-07-07 21:42:58 +0530

    Used Jwt sign method to get a token from the response

---
+++ backend-auth/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const app = require("./src/app");
+const connectDB = require('./src/db/db');
+
+
+connectDB();
+
+
+
+
+app.listen(2000, () => {
+    console.log('Server running on 2000');
+});
