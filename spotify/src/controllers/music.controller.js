a3347e95693e839192d5f37c9e8499a241ecd218
sanskar <191781814+anotherhalf004@users.noreply.github.com>
2026-07-09 01:15:03 +0530

    replaced with single try catch

--- spotify/src/controllers/music.controller.js
+++ spotify/src/controllers/music.controller.js
@@ -8,50 +8,46 @@ async function createMusic(req, res) {
     const token = req.cookies?.token;
 
     if (!token) {
-        return res.status(401).json({ message: 'Unauthorized/ Invalid token' });
-    }
-
-    let decoded;
-
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (decoded.role !== 'artist') {
-            return res.status(403).json({ message: 'You dont have access to create music' })
-        }
-    } catch (err) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    const { title } = req.body;
-    const file = req.file;
-
-    if (!file) {
-        return res.status(400).json({ message: 'Music file is required' });
-    }
-
-    let result;
-
     try {
-        result = await uploadFile(file.buffer.toString('base64'));
-    } catch (err) {
-        return res.status(500).json({ message: err.message || 'Failed to upload music' });
-    }
 
-    const music = await musicModel.create({
-        uri: result.url,
-        title,
-        artist: decoded.id
-    })
-
-    res.status(200).json({
-        message: 'Music created successfully',
-        music: {
-            id: music._id,
-            uri: music.uri,
-            title: music.title,
-            artist: music.artist
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (decoded.role !== 'artist') {
+            return res.status(403).json({ message: 'You dont have access to create music' })
         }
-    })
+
+        const { title } = req.body;
+        const file = req.file;
+
+        if (!file) {
+            return res.status(400).json({ message: 'Music file is required' });
+        }
+        
+        const result = await uploadFile(file.buffer.toString('base64'));
+
+        const music = await musicModel.create({
+            uri: result.url,
+            title,
+            artist: decoded.id
+        })
+
+        res.status(200).json({
+            message: 'Music created successfully',
+            music: {
+                id: music._id,
+                uri: music.uri,
+                title: music.title,
+                artist: music.artist
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({
+            message : 'Unauthorized'
+        })
+    }
 
 }
 
